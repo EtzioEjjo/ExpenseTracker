d725cd369e4513548b086c85571de2fc4dc9de79
EtzioEjjo <qq-21@outlook.com>
2023-08-26 21:11:14 +0300

    adjustment

--- src/main/java/com/example/abumusaapplication/App.java
+++ src/main/java/com/example/abumusaapplication/App.java
@@ -10,8 +10,8 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
 //        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/IronWeights.fxml"));
 //        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/PeopleClients.fxml"));
-        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/AddNewClient.fxml"));
-//        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/SoldToday.fxml"));
+//        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/AddNewClient.fxml"));
+        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/DailyRecord.fxml"));
 
 //        FXMLLoader loader= new FXMLLoader(getClass().getResource("/Fxml/BalanceInquiry.fxml"));
         Scene scene= new Scene(loader.load());
